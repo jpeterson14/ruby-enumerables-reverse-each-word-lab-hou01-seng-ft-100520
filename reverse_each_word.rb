5a0a5a0278d3ffbe5e1bde2b687479de4a67b4ee
Jake Peterson <jpeterson0514@gmail.com>
2020-09-04 23:06:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word (sentence)
-  sentence_array = sentence.split("")
+  sentence_array = sentence.split(" ")
   reversed_words_array = 
   sentence_array.collect do |word|
     word.reverse 
   end 
-  reversed_words_array.join("")
+  reversed_words_array.join(" ")
 end
